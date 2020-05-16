f9eb93f67d7434b7ba6d7380617c2054bfdcf526
justin smith <smithjn@g.cofc.edu>
2020-05-16 09:24:21 -0400

    init commit

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict'
+
+async function listRepos(username) {
+  const repos = await fetch(
+    `https://api.github.com/users/${username}/repos?type=owner&sort=updated`,
+  )
+    .then((res) => res.json())
+    .catch(console.error)
+
+  const markup = repos
+    .map(
+      (repo) => `
+    <li>
+      <a href="${repo.html_url}">${repo.name}</a>
+      (⭐ ${repo.stargazers_count})
+    </li>
+    `,
+    )
+    .join('')
+
+  const content = document.getElementById('content')
+  content.innerHTML = `<ul>${markup}</ul>`
+}
+
+listRepos('Jswizzy')
